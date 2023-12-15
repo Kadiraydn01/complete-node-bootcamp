728a03a798e5d4c26a1ed827a7da380ed5bec6bf
Kadiraydn01 <kadiraydn01@gmail.com>
2023-12-16 01:00:03 +0300

    learn node backend

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+// const textFs = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textFs);
+
+// const textOut = `This is what we know about the avocado: ${textFs}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//   });
+// });
+// console.log("Will read file!");
+const replaceTemp = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/login") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = productData.map((el) => replaceTemp(tempCard, el));
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+  } else if (pathName === "/register") {
+    res.end("Welcome to my register page");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>404 Page not found</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
